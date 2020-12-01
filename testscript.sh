ea9d72d6d66829e30328d2d83aa18760a6c0bd7e
sheshankyadav8 <sheshankyadav8@gmail.com>
2020-12-01 07:24:56 +0000

    added more line

--- testscript.sh
+++ testscript.sh
@@ -3,3 +3,7 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This was added"
+echo "this is funy"
+echo "this is hilarious"
+echo "this is cool tuck tuck !!"
+
